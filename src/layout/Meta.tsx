3e62aaa9d68b3448bc4f7b77a1ede0757d1c5842
Timo <timo@DE64LRLYW093C.fritz.box>
2022-07-17 23:10:40 +0200

    Add structured data example for faq page

--- src/layout/Meta.tsx
+++ src/layout/Meta.tsx
@@ -8,6 +8,7 @@ type IMetaProps = {
   title: string;
   description: string;
   canonical?: string;
+  structuredData: any;
 };
 
 const Meta = (props: IMetaProps) => {
@@ -58,97 +59,7 @@ const Meta = (props: IMetaProps) => {
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              '@context': 'http://schema.org',
-              '@type': ['SoftwareApplication', 'MobileApplication'],
-              applicationCategory: 'UtilitiesApplication',
-              name: 'EHW+ App',
-              description:
-                'Die Nebenkosten steigen. Haben Sie den Verbrauch im Blick? Mit der EHW+ Zählerstands-App schon.',
-              operatingSystem: ['Android', 'iOS', 'Windows', 'OSX', 'Linux'],
-              image: [
-                'https://image.winudf.com/v2/image1/Y29tLmVod3BsdXNfc2NyZWVuX3poLUNOXzFfMTYxMDIyNTI1OV8wOTA/screen-1.jpg?fakeurl=1&type=.jpg',
-              ],
-              brand: {
-                '@type': 'Brand',
-                name: 'EHW+',
-              },
-              offers: {
-                '@type': 'Offer',
-                price: '0',
-                priceCurrency: 'EUR',
-              },
-              email: 'mailto:info@ehwplus.com',
-              url: 'https://www.ehwplus.com',
-              review: [
-                {
-                  '@type': 'Review',
-                  reviewRating: {
-                    '@type': 'Rating',
-                    ratingValue: '5',
-                  },
-                  datePublished: '2022-01-15',
-                  reviewBody:
-                    'Eigentlich bleiben bei dieser App kaum Wünsche offen um den privaten Energieverbrauch Transparenz zu verleihen. Intuitive kurze Einarbeitung! Die wichtigsten Features kostenlos und trotzdem quasi keine Werbung. Sehr Übersichtlich offline nutzbar und an nahezu alle Verbrauchssituationen gedacht. Wer den Excel-Export braucht, für den sind einmalige 3,99 € für diese App absolut fair. Ob man für diese Zwecke ein Abo braucht? Sehe ich dann doch eher nicht. Aber alles in allem sehr gelungen. Danke!',
-                  author: {
-                    '@type': 'Person',
-                    name: 'Marco Hilbich',
-                  },
-                },
-                {
-                  '@type': 'Review',
-                  reviewRating: {
-                    '@type': 'Rating',
-                    ratingValue: '5',
-                  },
-                  datePublished: '2021-12-17',
-                  reviewBody:
-                    'Für mich perfekt. Sehr schnelle und intuitive Bedienung. Statistikkurve sehr übersichtlich. Hatte schon andere Apps für diesen Zweck. Alle deinstalliert da zu überladen und unübersichtlich. Zudem besteht die Möglichkeit der Mitgestaltung über ein Voting. Absolut empfehlenswert',
-                  author: {
-                    '@type': 'Person',
-                    name: 'Bernd Bratsch',
-                  },
-                },
-                {
-                  '@type': 'Review',
-                  reviewRating: {
-                    '@type': 'Rating',
-                    ratingValue: '5',
-                  },
-                  datePublished: '2021-05-31',
-                  reviewBody:
-                    'Tolle App für den Privatbereich, um Verbrauchsdaten im Überblick zu halten. Für mich sehr nützlich ist die Funktion, die Daten und Statistiken als Excel zu exportieren, um sie zb an Vermieter weiterzureichen. Ich kopiere die Daten in mein eigenes Excel template. Das ermöglicht mir geniale Auswertungsmöglichkeiten. Die App selbst ist übersichtlich gestaltet und hat einen professionellen Look. Positiv auch der nette Kontakt zum Entwickler. Berücksichtigt Wünsche und Anregungen der Nutzer! 👍',
-                  author: {
-                    '@type': 'Person',
-                    name: 'Carbon Racer',
-                  },
-                },
-                {
-                  '@type': 'Review',
-                  reviewRating: {
-                    '@type': 'Rating',
-                    ratingValue: '5',
-                  },
-                  datePublished: '2021-03-01',
-                  reviewBody:
-                    'Es macht Spaß diese App zu nutzen. Sie ist einfach zu verstehen und sehr schön übersichtlich. Für mich genau das Richtige. Ich nutze sie für Strom, Warmwasser und Heizung. Schon beim eingeben kann ich erkennen, ob ich mehr oder weniger verbraucht habe. Sehr hilfreich, da ich für letztes Jahr sehr viel nachzahlen musste. Der Kontakt zum Entwickler ist sehr angenehm. Probleme werden quasi sofort gelöst. Und das völlig entspannt und es wird alles sehr gut erklärt.',
-                  author: {
-                    '@type': 'Person',
-                    name: 'Takera',
-                  },
-                },
-              ],
-              aggregateRating: {
-                '@type': 'AggregateRating',
-                ratingValue: '4.8',
-                reviewCount: '312',
-                itemReviewed: {
-                  '@type': 'Thing',
-                  url: 'https://play.google.com/store/apps/details?id=com.ehwplus',
-                  alternateName: 'EHW+ Android',
-                },
-              },
-            }),
+            __html: JSON.stringify(props.structuredData),
           }}
         />
       </Head>
--- src/templates/Faq.tsx
+++ src/templates/Faq.tsx
@@ -5,9 +5,30 @@ import { TitleClaimAndAppStoreBadges } from './TitleClaimAndAppStoreBadges';
 
 // https://developers.google.com/search/docs/advanced/structured-data/faqpage?hl=de
 
+const q1 = 'Wie oft muss ich Zählerstände ablesen';
+const a1 =
+  '<p>Zählerstände müssen nur bei Start und Ende des Vertrags abgelesen werden. Um jedoch Ihren Verbrauch besser einschätzen zu können, sollten Sie alle 1 bis 3 Monate ablesen.</p>';
+
 const FaqPage = () => (
   <div className="antialiased text-gray-600">
-    <Meta title={AppConfig.title} description={AppConfig.description} />
+    <Meta
+      title={AppConfig.title}
+      description={AppConfig.description}
+      structuredData={{
+        '@context': 'https://schema.org',
+        '@type': 'FAQPage',
+        mainEntity: [
+          {
+            '@type': 'Question',
+            name: { q1 },
+            acceptedAnswer: {
+              '@type': 'Answer',
+              text: { a1 },
+            },
+          },
+        ],
+      }}
+    />
     <TitleClaimAndAppStoreBadges />
     <Footer />
   </div>
